684f1a67b2ef7e0a8f9fdb7159ad9a89ef6c7ebc
SofiRossyHC <sofiarociohernandezchavez@gmail.com.pe>
2024-09-17 12:01:03 -0500

    vbb

---
+++ CalcFxConhistorial/src/main/java/pe/edu/upeu/calcfx/servicio/CalcServiceI.java
@@ -0,0 +1,12 @@
+package pe.edu.upeu.calcfx.servicio;
+
+import pe.edu.upeu.calcfx.modelo.CalcTO;
+import java.util.List;
+
+public interface CalcServiceI {
+
+    public void guardarResultados (CalcTO to);//C
+    public List <CalcTO> obtenerResultados();//R
+    public void eliminarResultados (int index);//U
+    public void actualizarResultados (CalcTO to,int index);//D
+}
