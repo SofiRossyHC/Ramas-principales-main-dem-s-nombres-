c9aeb688941a3a217d66ca3c55b9c3f806982388
SofiRossyHC <sofiarociohernandezchavez@gmail.com>
2024-09-27 10:25:38 -0500

    Add files via upload

---
+++ 1/src/main/java/pe/edu/upeu/calcfx/servicio/CalcServiceImp.java
@@ -0,0 +1,35 @@
+package pe.edu.upeu.calcfx.servicio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import pe.edu.upeu.calcfx.dao.CalculadoraDao;
+import pe.edu.upeu.calcfx.modelo.CalcTO;
+
+import java.util.ArrayList;
+import java.util.List;
+@Service
+public class CalcServiceImp implements CalcServiceI{
+    @Autowired
+    CalculadoraDao dbOper;
+    //List<CalcTO> dbOper=new ArrayList<CalcTO>();
+    @Override
+    public void guardarResultados(CalcTO to) {
+       dbOper .insertar(to);
+    }
+
+    @Override
+    public List<CalcTO> obtenerResultados() {
+        return dbOper.listar();
+    }
+
+    @Override
+    public void eliminarResultados(int index) {
+        dbOper.eliminar(index);
+    }
+
+    @Override
+    public void actualizarResultados(CalcTO to, int index) {
+        dbOper.actualizar(to, index);
+    }
+}
